5a10d2484e4b4b7cd94561d7f3cbd1f00ee8320e
Palash Nath <palashnath880@gmail.com>
2024-08-09 15:09:20 +0600

    Component Types File Updated

--- src/types/component.types.ts
+++ src/types/component.types.ts
@@ -1,3 +1,5 @@
+import type { Group } from "./types";
+
 export type SearchFormProps = {
   placeholder: string;
   value?: string | undefined;
@@ -8,4 +10,16 @@ export type PageHeaderProps = {
   title: React.ReactNode;
   subtitle?: React.ReactNode;
   rightAction?: React.ReactNode;
+};
+
+export type DeleteMedicineGroupProps = {
+  group: Group;
+  refetch: () => void;
+};
+
+export type DeleteConfirmProps = {
+  open: boolean;
+  close: () => void;
+  title: string;
+  confirm?: () => void;
 };
