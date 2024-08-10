62c1a27df788e35d46032e9aa840dd2900a1574f
Palash Nath <palashnath880@gmail.com>
2024-08-10 21:47:50 +0600

    Medicine Generic Post Route Added

---
+++ src/app/api/medicine-generic/route.ts
@@ -0,0 +1,33 @@
+import prisma from "@/lib/prisma";
+import moment from "moment";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const generic = await req.json();
+
+    // get generic
+    const getGeneric = await prisma.generic.findUnique({
+      where: { name: generic?.name },
+    });
+
+    if (getGeneric) {
+      return NextResponse.json(
+        { message: `${generic?.name} already exists` },
+        { status: 409 }
+      );
+    }
+
+    // insert generic
+    const result = await prisma.generic.create({
+      data: {
+        name: generic?.name,
+        createdAt: moment().utc().toString(),
+      },
+    });
+
+    return NextResponse.json(result, { status: 201 });
+  } catch (err) {
+    return NextResponse.json(err, { status: 400 });
+  }
+}
