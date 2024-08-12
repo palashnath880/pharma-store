946ab893c8c1d1b8d024a87fde36b39656af9283
Palash Nath <palashnath880@gmail.com>
2024-08-12 21:55:57 +0600

    medicine get api route added

--- src/app/api/medicine/route.ts
+++ src/app/api/medicine/route.ts
@@ -28,7 +28,34 @@ export async function POST(req: NextRequest) {
     });
     return NextResponse.json(result, { status: 201 });
   } catch (err) {
-    console.log(err);
+    return NextResponse.json(err, { status: 400 });
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    const query = req.nextUrl.searchParams;
+    const search = query.get("search") || "";
+    const page = query.get("page") || "1";
+    const limit = query.get("limit") || "50";
+
+    const skip = (parseInt(page) - 1) * parseInt(limit);
+
+    // total count
+    const count = await prisma.medicine.count();
+
+    const data = await prisma.medicine.findMany({
+      skip: skip,
+      include: { generic: true },
+    });
+
+    const res = {
+      count,
+      data,
+    };
+
+    return NextResponse.json(res);
+  } catch (err) {
     return NextResponse.json(err, { status: 400 });
   }
 }
