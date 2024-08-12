26554a2e918be5f90c6630190b28bc6909805ff3
Palash Nath <palashnath880@gmail.com>
2024-08-12 19:31:22 +0600

    App Wrap By Redux Provider

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,11 +2,13 @@
 
 import Sidebar from "@/components/shared/Sidebar";
 import TopBar from "@/components/shared/Topbar";
+import { store } from "@/lib/store";
 import { Close } from "@mui/icons-material";
 import { IconButton, useTheme } from "@mui/material";
 import { closeSnackbar, SnackbarProvider } from "notistack";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
 
 const client = new QueryClient();
 
@@ -20,36 +22,38 @@ export default function Layout({
 
   return (
     <QueryClientProvider client={client}>
-      <SnackbarProvider
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-        maxSnack={5}
-        autoHideDuration={2000}
-        classes={{}}
-        action={(snackId) => (
-          <IconButton color="inherit" onClick={() => closeSnackbar(snackId)}>
-            <Close />
-          </IconButton>
-        )}
-      >
-        <style>
-          {`
+      <Provider store={store()}>
+        <SnackbarProvider
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          maxSnack={5}
+          autoHideDuration={2000}
+          classes={{}}
+          action={(snackId) => (
+            <IconButton color="inherit" onClick={() => closeSnackbar(snackId)}>
+              <Close />
+            </IconButton>
+          )}
+        >
+          <style>
+            {`
           .notistack-MuiContent-success{
             color: ${theme.palette.primary.main} !important
           }
         `}
-        </style>
-        <div className="w-screen h-screen">
-          <div className="flex h-full overflow-hidden">
-            <Sidebar />
-            <main className="flex-1 flex flex-col">
-              <TopBar />
-              <div className="flex-1 px-5 py-5 overflow-y-auto">
-                <div className="w-full h-full">{children}</div>
-              </div>
-            </main>
+          </style>
+          <div className="w-screen h-screen">
+            <div className="flex h-full overflow-hidden">
+              <Sidebar />
+              <main className="flex-1 flex flex-col">
+                <TopBar />
+                <div className="flex-1 px-5 py-5 overflow-y-auto">
+                  <div className="w-full h-full">{children}</div>
+                </div>
+              </main>
+            </div>
           </div>
-        </div>
-      </SnackbarProvider>
+        </SnackbarProvider>
+      </Provider>
     </QueryClientProvider>
   );
 }
